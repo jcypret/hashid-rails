3ebe00c2127337a06b9594d9ac2963a0518f4a33
Justin Cypret <jcypret@gmail.com>
2015-04-12 15:47:23 -0500

    remove unused specs

--- spec/hashid/rails_spec.rb
+++ spec/hashid/rails_spec.rb
@@ -4,8 +4,4 @@ describe Hashid::Rails do
   it 'has a version number' do
     expect(Hashid::Rails::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
