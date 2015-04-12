f1fc0aa9e843b6129f9723d01fbd4ff6bfec9aa3
Justin Cypret <jcypret@gmail.com>
2015-04-12 14:46:03 -0500

    initial commit

---
+++ lib/hashid/rails.rb
@@ -0,0 +1,7 @@
+require "hashid/rails/version"
+
+module Hashid
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ spec/hashid/rails_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Hashid::Rails do
+  it 'has a version number' do
+    expect(Hashid::Rails::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
