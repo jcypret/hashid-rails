d7f5bb931686064e06df0c669478f9f758c6f443
Justin Cypret <jcypret@gmail.com>
2015-04-12 15:09:57 -0500

    Add hashid methods to ActiveRecord::Base

--- lib/hashid/rails.rb
+++ lib/hashid/rails.rb
@@ -2,6 +2,35 @@ require "hashid/rails/version"
 
 module Hashid
   module Rails
-    # Your code goes here...
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def hashids
+        Hashids.new(table_name, 6)
+      end
+
+      def encode_id(id)
+        hashids.encode(id)
+      end
+
+      def decode_id(id)
+        hashids.decode(id).first
+      end
+
+      def hashid_find(hashid)
+        find(decode_id(hashid))
+      end
+    end
+
+    def encoded_id
+      self.class.encode_id(id)
+    end
+
+    def to_param
+      encoded_id
+    end
+    alias_method :hashid, :to_param
   end
 end
+
+ActiveRecord::Base.send :include, Hashid::Rails
