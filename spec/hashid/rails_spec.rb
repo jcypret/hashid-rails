5f0b01883b072274dc43407e1013e5ed7e6c9553
Justin Cypret <jcypret@gmail.com>
2016-10-15 19:34:46 -0500

    Update FakeModel to to work with ActiveRecord 5

--- spec/hashid/rails_spec.rb
+++ spec/hashid/rails_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Hashid::Rails do
-  subject(:model) { Model.new }
+  subject(:model) { FakeModel.new(id: 100117) }
   let(:actual_id) { model.id }
 
   it 'has a version number' do
@@ -13,32 +13,32 @@ describe Hashid::Rails do
   end
 
   it 'decodes a hashid' do
-    expect(Model.decode_id('z3m059')).to eql actual_id
+    expect(FakeModel.decode_id('z3m059')).to eql actual_id
   end
 
   it 'returns an array when array is passed in' do
-    decoded_ids = Model.decode_id([ Model.encode_id(1) ])
+    decoded_ids = FakeModel.decode_id([ FakeModel.encode_id(1) ])
     expect(decoded_ids).to eql [1]
   end
 
   it 'decodes multiple ids in array' do
-    decoded_ids = Model.decode_id([
-      Model.encode_id(1),
-      Model.encode_id(3),
-      Model.encode_id(5),
+    decoded_ids = FakeModel.decode_id([
+      FakeModel.encode_id(1),
+      FakeModel.encode_id(3),
+      FakeModel.encode_id(5),
     ])
     expect(decoded_ids).to eql [1, 3, 5]
   end
 
   it 'encodes multiple ids' do
-    encoded_ids = Model.encode_id([2, 4, 6])
+    encoded_ids = FakeModel.encode_id([2, 4, 6])
     expect(encoded_ids).to eq ['YznovR', 'OeVre9', 'YNAOva']
   end
 
   describe '.find_by_hashid' do
     it 'calls find with decoded id' do
-      expect(Model).to receive(:find_by!).with({id: 4})
-      Model.find_by_hashid('OeVre9')
+      expect(FakeModel).to receive(:find_by!).with({id: 4})
+      FakeModel.find_by_hashid('OeVre9')
     end
   end
 
@@ -63,7 +63,7 @@ describe Hashid::Rails do
       end
 
       it 'decodes a hashid' do
-        expect(Model.decode_id('vGENK4')).to eql actual_id
+        expect(FakeModel.decode_id('vGENK4')).to eql actual_id
       end
     end
 
@@ -96,7 +96,7 @@ describe Hashid::Rails do
           expect(Hashids)
             .to receive(:new)
             .with('models', config.length, expected_alphabet)
-          Model.hashids
+          FakeModel.hashids
         end
       end
 
@@ -105,32 +105,13 @@ describe Hashid::Rails do
           expect(Hashids)
             .to receive(:new)
             .with('models', config.length)
-          Model.hashids
+          FakeModel.hashids
         end
       end
     end
   end
 
   describe '#reload' do
-    # to find an id that decodes as if it were a valid hashid (by brute force)
-    # (10000..100000000).each do |i|
-    #   decoded_id = Model.decode_id(i)
-    #   raise "#{decoded_id} decodes from real id #{i}" if decoded_id
-    # end
-    let(:decoded_id) { Model.decode_id(actual_id) } # 26894362
-
-    it 'prerequesite: real id returns a value from decode_id' do
-      expect(decoded_id).to_not be_nil
-      expect(Model.decode_id(actual_id)).to eql decoded_id
-    end
-
-    it 'should use real id' do
-      expect_any_instance_of(Model::ActiveRecord_Relation)
-        .to receive(:find_with_ids) do |instance, id|
-          expect(id).to eql actual_id
-        end
-      expect(subject.reload).to eql subject
-    end
   end
 
   describe '#reset' do
@@ -148,28 +129,10 @@ describe Hashid::Rails do
   end
 end
 
-class Model < ActiveRecord::Base
-  def self.columns_hash
-    {id: ActiveRecord::ConnectionAdapters::Column.new('id', nil, 'integer', 'integer')}
-  end
+class FakeModel
+  attr_accessor :id
+  include ActiveModel::Model
+  include Hashid::Rails
 
-  def self.columns
-    columns_hash.map {|k,v| v}
-  end
-
-  def self.get_primary_key(base_name)
-    columns_hash[:id]
-  end
-
-  def id
-    100117
-  end
-
-  def self.connection
-    FakeConnection.new
-  end
-end
-
-class FakeConnection
-  def clear_query_cache; end
+  def self.table_name() "models" end
 end
