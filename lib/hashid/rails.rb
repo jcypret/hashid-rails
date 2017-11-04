ce4d33066d75ed63f7e3e204621ade13e04248c2
Justin Cypret <jcypret@gmail.com>
2017-11-03 22:29:03 -0500

    Disable rubocop name violation for has_many override

--- lib/hashid/rails.rb
+++ lib/hashid/rails.rb
@@ -38,7 +38,7 @@ module Hashid
         super.tap { |r| r.extend ClassMethods }
       end
 
-      def has_many(*args, &block)
+      def has_many(*args, &block) # rubocop:disable Style/PredicateName
         options = args.extract_options!
         options[:extend] = Array(options[:extend]).push(ClassMethods)
         super(*args, options, &block)
