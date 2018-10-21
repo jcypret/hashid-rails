3175f4b9a65aa3e8d1e6a049b6346051b74ebeae
Justin Cypret <jcypret@gmail.com>
2018-10-21 10:58:53 -0500

    Fix style violations (specs should still fail)

--- lib/hashid/rails.rb
+++ lib/hashid/rails.rb
@@ -83,8 +83,9 @@ module Hashid
       end
 
       def find_by_hashid!(hashid)
-          find_by!(id: decode_id(hashid))
+        find_by!(id: decode_id(hashid))
       end
+
       private
 
       def hashids
